1990273b9385660e57df75ac8ba129ee68619402
ohunyanc <wildcardcharly@outlook.com>
2024-08-20 06:05:09 +0100

    commit

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function LoadingPage(){
     return (
-        <div className="flex items-center justify-center h-[380px] md:h-[500px]">
+        <div className="flex items-center justify-center h-[480px] md:h-[500px]">
             <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-emerald-900/50"/>
         </div>
     )
