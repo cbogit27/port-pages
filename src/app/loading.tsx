9afaf7081aa0b503158e8a67c3df4ba521e90c87
cbogit27 <yescharly27@gmail.com>
2024-12-07 01:48:12 +0100

    Updates

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingPage(){
-    return (
-        <div className="flex items-center justify-center h-[1200px] lg:h-[900px]">
-            <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-emerald-900/50"/>
-        </div>
-    )
-}
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-// src/types.ts
-export type FormData = {
-    name: string;
-    email: string;
-    message: string;
-  };
-  
